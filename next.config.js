60b7b4aa4c95de357be321c8970830b811875a28
Dušan Neckař <neckar.dusan@seznam.cz>
2023-11-14 22:08:17 +0100

    change next config

--- next.config.js
+++ next.config.js
@@ -3,10 +3,14 @@ const withNextra = require('nextra')({
   themeConfig: './theme.config.js',
   // optional: add `unstable_staticImage: true` to enable Nextra's auto image import
 })
-
+const repo = 'portfolio'
+const assetPrefix = `/${repo}/`
+const basePath = `/${repo}`
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // any configs you need
+  basePath: basePath,
+  assetPrefix: assetPrefix,
   output: "export",
   distDir: "dist",
   images: {
